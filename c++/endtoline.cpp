77dc3f7dd696c44c7622b9146e8c270c9669f32d
Sathira Pramudith <sathirapramudith1@gmail.com>
2025-12-18 19:25:10 +0530

    commit ok

---
+++ c++/endtoline.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    // this one method
+    std::cout<<"I like pizza!"<<std::endl;
+    std::cout<<"It's really good!"<<std::endl;
+
+
+    // this is another method
+    std::cout<<"I like pizza!"<<'\n';
+    std::cout<<"It's really good!"<<'\n';
+    return 0;
+}
---
+++ c++/pizza.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+    std::cout<<"i like a pizza";
+    return 0;
+}
